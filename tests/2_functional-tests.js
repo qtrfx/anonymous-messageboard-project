c0391a544cbeea9d3f82e85875fc2c9fb7e027c7
qtrfx <24359705+qtrfx@users.noreply.github.com>
2023-07-23 00:41:44 +0200

    Add testing

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,6 +6,10 @@ const expect = chai.expect;
 let mappedArr;
 let threadId;
 let threadDeletePassword;
+let testTextString = btoa((Math.random() * 10000).toString())
+
+let replyString = btoa((Math.random() * 10000).toString())
+let replyId;
 
 chai.use(chaiHttp);
 
@@ -17,14 +21,14 @@ suite('Functional Tests', () => {
       .keepOpen()
       .post("/api/threads/testing")
       .type('form')
-      .send({text: 'testing text', delete_password: 'test'})
+      .send({text: testTextString, delete_password: 'test'})
       .end((err, res) => {
-        console.log(res)
         assert.equal(res.status, '200')
         expect(res).to.redirectTo(`http://localhost:3001/b/testing/`)
         done();
       
       })
+
       })
       test('View the 10 most recent threads', function(done) {
         chai
@@ -34,21 +38,133 @@ suite('Functional Tests', () => {
       .end((err, res) => {
         assert.equal(res.status, '200')
         assert.isAtMost(res.body.length, 10)
+        assert.isAtLeast(res.body.length, 1)
+        res.body.forEach(e => {
+          if(e.text == testTextString) {
+            threadId = e._id
+          }
+          assert.isAtMost(e.replies.length, 3)})
         done();
       
       })
 
 
       })
-      test('View the 10 most recent threads', function(done) {
+      
+      test('Report a thread', function(done) {
         chai
       .request('http://localhost:3001')
       .keepOpen()
-      .get("/api/threads/testing")
+      .put("/api/threads/testing")
+      .type('form')
+      .send({threadId: threadId })
       .end((err, res) => {
-        const yepper = res.body.map(e => e.replies.length)
-        res.body.forEach(e => assert.isAtMost(e.replies.length, 3))
         assert.equal(res.status, '200')
+        assert.equal(res.text, 'reported')
+        done();
+      
+      })
+
+
+      })
+      test('Create a reply', function(done) {
+        chai
+      .request('http://localhost:3001')
+      .keepOpen()
+      .post(`/api/replies/testing`)
+      .type('form')
+      .send({delete_password: 'lol', thread_id: threadId, text: replyString })
+      .end((err, res) => {
+        const url = 'http://localhost:3001/b/testing/' + threadId
+        assert.equal(res.status, '200')
+        expect(res).to.redirectTo(url)
+        done();
+      
+      })
+
+
+      })
+      test('Viewing all replies of a thread', function(done) {
+        chai
+      .request('http://localhost:3001')
+      .keepOpen()
+      .get(`/api/replies/testing?thread_id=${threadId}`)
+      .end((err, res) => {
+        res.body.replies.forEach(reply => {
+          if(reply.text == replyString) {
+            replyId = reply._id
+          }
+        })
+        assert.isObject(res.body)
+        assert.property(res.body, '_id')
+        assert.property(res.body, 'board')
+        assert.property(res.body,'text')
+        assert.equal(res.body.board, 'testing')
+        assert.equal(res.body.text, testTextString)
+        assert.property(res.body, 'created_on')
+        assert.property(res.body, 'bumped_on')
+        assert.property(res.body,'replycount')
+        assert.property(res.body, 'replies')
+        res.body.replies.forEach(reply => {
+          assert.property(reply, '_id')
+          assert.property(reply, 'text')
+          assert.property(reply, 'created_on')
+
+          if(reply._id == replyId) {
+            assert.equal(reply.text, replyString)
+          }
+        })
+        done();
+      
+      })
+
+
+      })
+      test('Report a reply', function(done) {
+        chai
+      .request('http://localhost:3001')
+      .keepOpen()
+      .put(`/api/replies/testing`)
+      .type('form')
+      .send({thread_id: threadId, reply_id: replyId})
+      .end((err, res) => {
+        assert.equal(res.status, 200)
+        assert.equal(res.text, 'reported')
+       
+        done();
+      
+      })
+
+
+      })
+      test('Delete a reply with incorrect password', function(done) {
+        chai
+      .request('http://localhost:3001')
+      .keepOpen()
+      .delete(`/api/replies/testing`)
+      .type('form')
+      .send({thread_id: threadId, reply_id: replyId, delete_password: 'incorrect'})
+      .end((err, res) => {
+        assert.equal(res.status, 200)
+        assert.equal(res.text, 'incorrect password')
+       
+        done();
+      
+      })
+
+
+      })
+      test('Delete a reply with correct password', function(done) {
+        chai
+      .request('http://localhost:3001')
+      .keepOpen()
+      .delete(`/api/replies/testing`)
+      .type('form')
+      .send({thread_id: threadId, reply_id: replyId, delete_password: 'lol'})
+      .end((err, res) => {
+        assert.equal(res.status, 200)
+        assert.equal(res.text, 'success')
+       
         done();
       
       })
@@ -60,17 +176,33 @@ suite('Functional Tests', () => {
       .request('http://localhost:3001')
       .keepOpen()
       .delete("/api/threads/testing")
+      .type('form')
+      .send({delete_password: 'dgshfjgfjhk', threadId: threadId })
       .end((err, res) => {
-        const yepper = res.body.map(e => e.replies.length)
-        res.body.forEach(e => assert.isAtMost(e.replies.length, 3))
         assert.equal(res.status, '200')
+        assert.equal(res.text, 'incorrect password')
         done();
       
       })
 
 
       })
+      test('Delete Thread with correct Password', function(done) {
+        chai
+      .request('http://localhost:3001')
+      .keepOpen()
+      .delete("/api/threads/testing")
+      .type('form')
+      .send({delete_password: 'test', threadId: threadId })
+      .end((err, res) => {
+        assert.equal(res.status, '200')
+        assert.equal(res.text, 'success')
+        done();
+      
+      })
 
+
+      })
   
 
 });
